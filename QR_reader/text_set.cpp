6d24149642bf1ceae2b710eaa7e91bd42f62b77f
Iwabuchi Takamasa <16403.nit@gmail.com>
2018-06-21 22:16:59 +0900

    QRコードのデータを整形してファイルに保存

--- QR_reader/text_set.cpp
+++ QR_reader/text_set.cpp
@@ -1,10 +1,16 @@
 #include "text_set.h"
 
-void node_set(TextWriter info, Array<String> node) {
+void node_set(TextWriter info, Array<String> node) {	//nodeには整形済みデータが入る
 	String buf;
 
 	for (const auto vertex : node) {
-		buf = vertex.substr(vertex.indexOf(L" ") + 1);
-		info.writeln(buf);
+		//buf = vertex.substr(vertex.indexOf(L" ") + 1);
+		if (vertex.front() == ' ') {	//なぜか半角コロンの後ろにスペースを含んでいるときのスペース除去
+			buf = vertex.substr(1);	//2番目の文字から最後まで（最後は省略）
+		}
+		else {
+			buf = vertex;
+		}
+		info.writeln(buf);	//実際にファイルへの書き込み処理？
 	}
 }
